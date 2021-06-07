962b2384067a0591751b99f6ceb02c87f621b33e
sidhu70 <83882188+sidhu70@users.noreply.github.com>
2021-06-07 12:13:39 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Yone Robot
+TITLE Sidhu Robot
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m YoneRobot
+env\scripts\activate.bat && py -m sidhu-Robot
